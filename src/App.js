c017ede639c243953a7cb5fd79208261897486d0
Yunsu <kenatman0723@gmail.com>
2021-05-21 11:57:05 +0900

    useClick

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { useState, useEffect, useRef } from "react";
 import { useInput } from "./useInput";
 import { useTabs } from "./useTabs";
 import { useTitle } from "./useTitle";
+import { useClick } from "./useClick";
 
 const App = () => {
-  const potato = useRef();
-  setTimeout(() => potato?.current?.focus(), 2000);
+  const sayHello = () => {
+    console.log(`hello`);
+  };
+  const title = useClick(sayHello);
   return (
     <div>
-      <span>Hi</span>
-      <input ref={potato} placeholder="Write sth..." />
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 };
