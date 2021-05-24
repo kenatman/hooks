540ceb123fba614a4ad1f3796ee9908ce457ff65
Yunsu <kenatman0723@gmail.com>
2021-05-24 17:13:53 +0900

    useFadeIn

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ import { useConfirm } from "./useConfirm";
 import { usePreventLeave } from "./usePreventLeave";
 import { useBeforeLeave } from "./useBeforeLeave";
 
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.opacity = 1;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
+
 const App = () => {
-  const begForLife = () =>
-    console.log(`Dont leave!! you can get discount coupon!!`);
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(2, 1);
+  const fadeInP = useFadeIn(3, 1);
   return (
     <div>
-      <h1>hello</h1>
+      <h1 {...fadeInH1}>hello</h1>
+      <p {...fadeInP}>This is the story!!</p>
     </div>
   );
 };
