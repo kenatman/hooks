a913aa0f3f1ec3d982b6ce4b39cd160f31093b65
Yunsu <kenatman0723@gmail.com>
2021-05-25 16:26:18 +0900

    useFullScreen

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ import { usePreventLeave } from "./usePreventLeave";
 import { useBeforeLeave } from "./useBeforeLeave";
 import { useNetwork } from "./useNetwork";
 import { useScroll } from "./useScroll";
+import { useFullScreen } from "./useFullScreen";
 
 const App = () => {
-  const { y } = useScroll();
+  const onFullS = (isFull) => {
+    console.log(isFull ? "FULL" : "SMALL");
+  };
+  const { element, triggerFull, exitFull } = useFullScreen(onFullS);
   return (
     <div style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
-        Hello
-      </h1>
+      <div ref={element}>
+        <img
+          alt="panda"
+          src="https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuZGF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        />{" "}
+        <button onClick={exitFull}>Exit FullScreen</button>
+      </div>
+
+      <button onClick={triggerFull}>Make FullScreen</button>
     </div>
   );
 };
