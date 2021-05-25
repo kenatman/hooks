f8d34b60d3e9240ef71d1122f628ee8b14971604
Yunsu <kenatman0723@gmail.com>
2021-05-25 17:41:26 +0900

    useNotification

--- src/App.js
+++ src/App.js
@@ -10,23 +10,15 @@ import { useBeforeLeave } from "./useBeforeLeave";
 import { useNetwork } from "./useNetwork";
 import { useScroll } from "./useScroll";
 import { useFullScreen } from "./useFullScreen";
+import { useNotification } from "./useNotification";
 
 const App = () => {
-  const onFullS = (isFull) => {
-    console.log(isFull ? "FULL" : "SMALL");
-  };
-  const { element, triggerFull, exitFull } = useFullScreen(onFullS);
+  const triggerNotif = useNotification(`Can I get Kimchi?`, {
+    body: `Kimchi is healthy!!`,
+  });
   return (
     <div style={{ height: "1000vh" }}>
-      <div ref={element}>
-        <img
-          alt="panda"
-          src="https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuZGF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-        />{" "}
-        <button onClick={exitFull}>Exit FullScreen</button>
-      </div>
-
-      <button onClick={triggerFull}>Make FullScreen</button>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 };
---
+++ src/useNotification.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"; // if you don't use it, it's not hooks
+
+export const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === `granted`) {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
